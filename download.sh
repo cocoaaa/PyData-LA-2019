8ea95d9045c56f82f5323070a19f0e6ad080262f
Hayley Song <haejinso@usc.edu>
2019-12-03 02:30:45 -0800

    updates to download pretrained model and aoi_vegas sample dataset from google drive

--- download.sh
+++ download.sh
@@ -10,6 +10,15 @@ tar -xvf cocolike.tar.gz
 
 # Download samples
 # gdown https://drive.google.com/uc?id=1dNYHuMmnOuU3H9fniuxGisYMOYeSUwbR
+cd sample
+gdown https://drive.google.com/uc?id=1D9cmDlfhpLTswWoJYAf4vqb85cjDnKrm
+tar -xvf AOI_2_Vegas_Roads_Sample.tar.gz 
+
 
 # If gdown doesn't work, try this
 # wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QiqUCEt8uX0YdPUm7jCjEBu8M_LWz3Vk' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O FILENAME && rm -rf /tmp/cookies.txt
+
+
+# Download model
+cd ../../pretrained 
+gdown https://drive.google.com/uc?id=1XwKd_2bOFmPG50QH7eetCpQAd0KUnjh3
